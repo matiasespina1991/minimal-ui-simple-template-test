cab1aa352480f552fae31b03ff54a8e210f4d6dd
Matias Espina <matiasespina1991@gmail.com>
2022-01-10 22:55:00 -0300

    project demo ready - empty

---
+++ src/layouts/dashboard/navbar/NavConfig.tsx
@@ -0,0 +1,50 @@
+// components
+import SvgIconStyle from '../../../components/SvgIconStyle';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name: string) => (
+  <SvgIconStyle src={`/icons/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const ICONS = {
+  user: getIcon('ic_user'),
+  ecommerce: getIcon('ic_ecommerce'),
+  analytics: getIcon('ic_analytics'),
+  dashboard: getIcon('ic_dashboard'),
+  calendar: getIcon('ic_calendar')
+};
+
+const sidebarConfig = [
+  // GENERAL
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'menu',
+    items: [
+      { title: 'Dashboard', path: '/dashboard/main', icon: ICONS.dashboard },
+      { title: 'Purchases', path: '/dashboard/purchases', icon: ICONS.ecommerce },
+      { title: 'Dates', path: '/dashboard/dates', icon: ICONS.calendar },
+      { title: 'Statics', path: '/dashboard/statics', icon: ICONS.analytics },
+    ],
+  },
+
+  // MANAGEMENT
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'advanced settings',
+    items: [
+      {
+        title: 'preferences',
+        path: '/dashboard/preferences',
+        icon: ICONS.user,
+        children: [
+          { title: 'Appearence', path: '/dashboard/preferences/appearence' },
+          { title: 'Privacy and security', path: '/dashboard/preferences/privacy-and-security' },
+          { title: 'System', path: '/dashboard/preferences/system' },
+        ],
+      },
+    ],
+  },
+];
+
+export default sidebarConfig;
